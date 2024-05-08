69aa527c69cc11f434b6a3273b310cac74978c57
Meet <91799@Meet>
2024-05-09 00:10:02 +0530

    conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import WeatherApp from "./WeatherApp";
+// import WeatherApp from "./WeatherApp";
+import TodoTab from "../../../Todo_List/todo_list/src/todoTab";
 function App() {
  return(
   <>
-   <WeatherApp></WeatherApp>
+   {/* <WeatherApp></WeatherApp> */}
+   <TodoTab></TodoTab>
+   
    </>
  );
 }
